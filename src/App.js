b83d9b180b5bcd5d72bc5360e9cb4fac061dfa25
jonathans0ramirez <jonathan.ramirez@experimentality.co>
2021-03-13 13:58:13 -0500

    project schema

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import Layout from './HOC/Layout/Layout';
+import Home from './containers/Home/Home';
+import List from './containers/List/List';
+import './App.sass';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Switch>
+        <Redirect exact from='/' to='/home' />
+        <Route path="/home" exact component={Home} />
+        <Route path="/list" component={List} />
+        <Route path="*" render={() => <p>NO TENGO NADA</p>} />
+      </Switch>
+    </Layout>
   );
 }
 
